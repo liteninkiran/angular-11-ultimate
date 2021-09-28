6f3c1e01205cf08a194f6130d66e06a619ea2e90
Kiran Anand <liteninkiran@hotmail.com>
2021-09-28 18:20:44 +0100

    Moving navbar to component

--- TaskManager/src/app/app.module.ts
+++ TaskManager/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, AboutComponent],
+  declarations: [AppComponent, DashboardComponent, AboutComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
